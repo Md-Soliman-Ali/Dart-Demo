412e88663752e8fe5857d6958e6f0bd4bd22d0fb
Md. Soliman Ali <soliman15-8572@diu.edu.bd>
2025-03-18 16:51:24 +0600

    remove unnecessary files

--- dart_interactive_cares/test/dart_interactive_cares_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_interactive_cares/dart_interactive_cares.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
--- dart_ostad/test/dart_ostad_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_ostad/dart_ostad.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
