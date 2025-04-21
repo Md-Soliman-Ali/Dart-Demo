0265153240e5336ff814e6225f709bdf7e422dfd
Md. Soliman Ali <soliman15-8572@diu.edu.bd>
2025-04-21 14:49:00 +0600

    List

---
+++ dart_interactive_cares/bin/ashif/recordClass/2. List.dart
@@ -0,0 +1,10 @@
+void main() {
+  List<String> myFriend = ["Sujan", "Shakil", "Shuvro"];
+  print(myFriend);
+  myFriend.add("Rocky");
+  print(myFriend);
+  myFriend.insert(0, "Arunav");
+  print(myFriend);
+  print(myFriend.indexOf("Shakil"));
+  print(myFriend[1]);
+}
